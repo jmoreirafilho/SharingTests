7dfea8e6bd48b8b09ceb46360df27d93787078c3
Airton <jmoreira@iget.com.br>
2015-08-14 13:40:21 -0300

    creating models

---
+++ app/Models/Course.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'courses';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Indicates if the model should be timestamped
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ app/Models/Teacher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'teachers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Indicates if the model should be timestamped
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
