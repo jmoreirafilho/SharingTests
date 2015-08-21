34cbdc918933b444164344e519f9721a43eb4a93
Airton <jmoreira@iget.com.br>
2015-08-20 21:06:45 -0300

    fix changes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,9 +3,14 @@
 
 @section('content')
 <section>
-	<div class="col-md-12">
+	<div class="col-md-7">
 		<div class="card">
 			<div class="card-title">@lang('title.home-index')</div>
+		</div>
+	</div>
+	<div class="col-md-5">
+		<div class="card">
+			<div class="card-title">Sobre</div>
 		</div>
 	</div>
 </section>
